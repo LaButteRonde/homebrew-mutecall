899b88b11973bf7c4f03d6c2682aed01f8e0ee91
PradalCyril <pradal.cyril@gmail.com>
2026-04-02 09:00:08 +0200

    update mutecall

--- Casks/mutecall.rb
+++ Casks/mutecall.rb
@@ -1,13 +1,19 @@
 cask "mutecall" do
   version "1.11.3"
-  sha256 "2e82a38bf3bf24439dda6b125c846e5644404f58fb05fc810e493eaa64c5bb53"
-
+  sha256 "..."
+  
   url "https://github.com/LaButteRonde/homebrew-mutecall/releases/download/v#{version}/MuteCall-darwin-arm64-#{version}.zip"
   name "MuteCall"
   desc "Global mute/unmute shortcut for Microsoft Teams on macOS"
   homepage "https://github.com/LaButteRonde/homebrew-mutecall"
 
   app "MuteCall.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+      args: ["-dr", "com.apple.quarantine", "#{appdir}/MuteCall.app"],
+      sudo: false
+  end
 
   zap trash: [
     "~/Library/Application Support/mutecall",
